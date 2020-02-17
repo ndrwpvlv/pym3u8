0b63d474a389264ff8d35b742907f9b18eec5126
ndrwpvlv <ndrw.pvlv@gmail.com>
2020-02-17 19:51:35 +0300

    Minor updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 with open('requirements.txt') as fr:
@@ -10,18 +10,22 @@ with open('requirements.txt') as fr:
 
 setup(
     name='pym3u8',
-    version='0.0.1',
+    version='0.1.1',
     packages=['pym3u8', ],
     url='https://github.com/ndrwpvlv/pym3u8',
-    license='',
+    license='MIT',
     author='Andrei S. Pavlov',
     author_email='ndrw.pvlv@gmail.com',
     description='Simple Python 3 lib for downloading online HLS streams and videos',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
+    keywords=['M3U8', 'HLS', 'STREAM', 'VIDEO', ],
+
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
     ],
     install_requires=required,
 )
